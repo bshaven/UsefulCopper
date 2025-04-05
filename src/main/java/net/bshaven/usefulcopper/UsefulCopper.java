17b9ab24e758232380f5c0d6660d1104c86dadc0
Brandon <odancraft@gmail.com>
2025-04-05 07:41:07 -0400

    Initial commit

---
+++ src/main/java/net/bshaven/usefulcopper/UsefulCopper.java
@@ -0,0 +1,16 @@
+package net.bshaven.usefulcopper;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UsefulCopper implements ModInitializer {
+	public static final String MOD_ID = "usefulcopper";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/bshaven/usefulcopper/UsefulCopperClient.java
@@ -0,0 +1,10 @@
+package net.bshaven.usefulcopper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UsefulCopperClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
