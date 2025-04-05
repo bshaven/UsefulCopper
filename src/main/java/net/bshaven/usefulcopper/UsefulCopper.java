b58093a370bda55b6ab51e914953635a46170ced
Brandon <odancraft@gmail.com>
2025-04-05 08:11:23 -0400

    Added two new items. - Pink Garnet - Raw Pink Garnet

--- src/main/java/net/bshaven/usefulcopper/UsefulCopper.java
+++ src/main/java/net/bshaven/usefulcopper/UsefulCopper.java
@@ -1,5 +1,6 @@
 package net.bshaven.usefulcopper;
 
+import net.bshaven.usefulcopper.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class UsefulCopper implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
