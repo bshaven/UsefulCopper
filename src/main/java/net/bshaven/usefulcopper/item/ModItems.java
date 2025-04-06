27831787ee3cb1002b3c10ea0b3a29e07ab127eb
Brandon <odancraft@gmail.com>
2025-04-06 07:56:19 -0400

    Testing out a custom item (chisel) Will probably remove.

--- src/main/java/net/bshaven/usefulcopper/item/ModItems.java
+++ src/main/java/net/bshaven/usefulcopper/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.bshaven.usefulcopper.item;
 
 import net.bshaven.usefulcopper.UsefulCopper;
+import net.bshaven.usefulcopper.item.custom.ChiselItem;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -12,6 +13,8 @@ public class ModItems {
 
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    public static final Item CHISEL = registerItem("chisel", new ChiselItem(new Item.Settings().maxDamage(32)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(UsefulCopper.MOD_ID, name), item);
